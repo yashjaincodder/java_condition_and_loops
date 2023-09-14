d97959299a4a45dd949975762faad5be3ac84d9a
Yash Jain <105738560+yashjaincodder@users.noreply.github.com>
2023-09-14 23:59:58 +0530

    Add files via upload

---
+++ HcDandLcf.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class HcDandLcf {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int gcd=1;
+        for(int i=1;i<a&&i<b;i++){
+            if((a%i==0)&&(b%i==0)){
+              gcd=i;
+            }
+        }
+        System.out.println("hcf is "+gcd);
+        int lcf=(a*b)/gcd;
+        System.out.println("lcm is "+lcf);
+       } 
+}
